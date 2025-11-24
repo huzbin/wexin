05f5a13f480343b5eacbd8fa5950dffbc36a2938
hubb <90473020+huzbin@users.noreply.github.com>
2025-11-25 01:00:34 +0800

    Add files via upload

---
+++ insight-front-end/src/components/index.js
@@ -0,0 +1,9 @@
+import Robot from './Robot';
+import RobotInfo from './RobotInfo';
+import Task from './Task';
+import TaskForm from './TaskForm';
+import Log from './Log';
+
+export {
+  Robot, RobotInfo, Task, TaskForm, Log,
+};
